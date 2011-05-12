7d894c2f5ccd7e26c4d4506132bc60f4bed091ec
James Halliday <mail@substack.net>
2011-05-11 21:57:03 -0700

    passing test for .peek()

--- test/binary.js
+++ test/binary.js
@@ -779,4 +779,42 @@ exports.intoStream = function () {
             clearTimeout(to);
         })
     ;
-}
+};
+
+exports.peek = function () {
+    var to = setTimeout(function () {
+        assert.fail('never finished');
+    }, 500);
+    
+    var bufs = [
+        new Buffer([ 6, 1, 6, 1, 6, 9, 0, 0, 0, 97 ]),
+        new Buffer([ 98, 99 ]),
+        new Buffer([ 100, 101, 102 ]),
+    ];
+    
+    var stream = new EventEmitter;
+    var iv = setInterval(function () {
+        var buf = bufs.shift();
+        if (buf) stream.emit('data', buf)
+        else clearInterval(iv)
+    }, 20);
+    
+    Binary.stream(stream)
+        .buffer('sixone', 5)
+        .peek(function () {
+            this.word32le('len');
+        })
+        .buffer('buf', 'len')
+        .word8('x')
+        .tap(function (vars) {
+            clearTimeout(to);
+            assert.eql(vars.sixone, new Buffer([ 6, 1, 6, 1, 6 ]));
+            assert.eql(vars.buf.length, vars.len);
+            assert.eql(
+                [].slice.call(vars.buf),
+                [ 9, 0, 0, 0, 97, 98, 99, 100, 101 ]
+            );
+            assert.eql(vars.x, 102);
+        })
+    ;
+};
